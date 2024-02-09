ab0edb8abb4cf136de8bc3b2fe7bc21c02cdbc94
vchepovskii <vadim.chepovsky@booking.com>
2024-02-09 15:17:05 +0100

    Remove legacy "dropped frames" metric

--- perfsuite/src/main/java/com/booking/perfsuite/rendering/RenderingMetrics.kt
+++ perfsuite/src/main/java/com/booking/perfsuite/rendering/RenderingMetrics.kt
@@ -22,17 +22,6 @@ public data class RenderingMetrics(
     val frozenFrames: Long,
 
     /**
-     * Amount of "good" frames that potentially could be rendered, but were "dropped" due to
-     * rendering performance issues.
-     *
-     * For instance if we target 60fps, the "good" frame would be any frame rendered for less than
-     * 16ms. But when we see "slow" frame that take 48ms to render, it spend the same time to render
-     * as 3 "good" frames would spend. That means that due to performance issue the app has rendered
-     * 1 frame, but 2 potentially good frames were dropped
-     */
-    val droppedFrames: Long,
-
-    /**
      * Total time of freezing the UI due to rendering of the slow frames per screen session.
      * This metric accumulates all freeze durations during the screen session
      */
@@ -44,4 +33,3 @@ public data class RenderingMetrics(
      */
     val foregroundTimeMs: Long? = null
 )
-
