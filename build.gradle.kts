5a67bbd40d4acb6a444ccd66eef1bb872e94c5ee
vchepovskii <vadim.chepovsky@booking.com>
2024-02-12 10:41:55 +0100

    Add Maven-publish plaging config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    id("com.vanniktech.maven.publish") version "0.25.3" apply false
 }
--- perfsuite/build.gradle.kts
+++ perfsuite/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin)
     alias(libs.plugins.android.library)
+    id("com.vanniktech.maven.publish")
 }
 
 kotlin {
@@ -25,4 +28,47 @@ android {
 dependencies {
     implementation(libs.androidx.ktx)
     implementation(libs.androidx.fragment)
+}
+
+// publishing
+
+val libName = "PerfSuite"
+val libDescription = "Lightweight library for collecting app performance metrics"
+val libUrl = "https://github.com/bookingcom/perfsuite-android"
+
+
+val groupId = project.group as String
+val artifactId = project.name as String
+val version = project.version as String
+
+@Suppress("UnstableApiUsage")
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.DEFAULT, true)
+    signAllPublications()
+
+    coordinates(groupId, artifactId, version)
+
+    pom {
+        name.set(libName)
+        description.set(libDescription)
+        url.set(libUrl)
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        scm {
+            connection.set("scm:git:github.com/bookingcom/perfsuite-android.git")
+            developerConnection.set("scm:git:ssh://github.com/bookingcom/perfsuite-android.git")
+            url.set("https://github.com/bookingcom/perfsuite-android")
+        }
+        developers {
+            developer {
+                name.set("Vadim Chepovskii")
+                email.set("smbduknow@gmail.com")
+            }
+        }
+    }
 }
